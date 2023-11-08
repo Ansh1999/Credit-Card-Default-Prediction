5f4da9a57194a48a71471abc92b4228d556a3956
Ansh1999 <124252808+Ansh1999@users.noreply.github.com>
2023-11-08 18:03:55 +0530

    Update application.py

--- application.py
+++ application.py
@@ -42,4 +42,5 @@ def predict_datapoint():
         return render_template('result.html',results=predicted)
 
 
-app.run(debug=True,host='0.0.0.0',port=5000)
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
